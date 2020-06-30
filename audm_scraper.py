dc41f68e164ea89ad3fa5a059b0388c470fd99c9
jim zhou <43537315+jimtje@users.noreply.github.com>
2020-06-30 03:49:48 -0700

    PEP8

--- audm_scraper.py
+++ audm_scraper.py
@@ -1,8 +1,9 @@
-import requests
-import os
-import taglib
-import shutil
 import configparser
+import os
+import shutil
+
+import requests
+import taglib
 from alive_progress import alive_bar
 
 config = configparser.RawConfigParser()
@@ -60,7 +61,10 @@ class Audm(object):
         return r.json()["result"]
 
     def articles(self, publication_ids=[], narrator_names=[], author_names=[]):
-        payload = {"publication_ids":publication_ids,"ordering":"byAudmDateDesc","narrator_names":narrator_names,"author_names":author_names}
+        payload = {
+                "publication_ids": publication_ids, "ordering": "byAudmDateDesc", "narrator_names": narrator_names,
+                "author_names": author_names
+        }
         url = "https://api.audm.com/v2/prefetchMinimumDiscoverScreenDataForArticleList"
         r = self.session.post(url, json=payload)
         return r.json()["result"]
@@ -90,8 +94,6 @@ def main():
     # This grabs the filtering options available. Filtering is first done by narrator, publication, or author.
     filters = audm.filters()
 
-
-
     for eachpublication in filters["publications"]:
         print("Publication: " + eachpublication["name_full"])
 
@@ -118,7 +120,8 @@ def main():
                 p = audm.paragraphs([article["object_id"]])
                 os.makedirs(publications_dir + "/" + article["short_name"], exist_ok=True)
 
-                # Articles are split up by paragraph and there can be quite a few. Although they are numbered and timestamped, makes more sense to join them
+                # Articles are split up by paragraph and there can be quite a few. Although they are numbered and
+                # timestamped, makes more sense to join them
                 with alive_bar(len(p), force_tty=True) as filebar:
                     for f in p:
                         file = audm.get_file(f["audio_filename"])
@@ -127,11 +130,10 @@ def main():
                             fz.write(file.content)
                             files.append({"filename": filename, "index": f["index"]})
                         filebar()
-                fo = sorted(files, key = lambda i: i['index'])
+                fo = sorted(files, key=lambda i: i['index'])
                 # Temporary file to enable ffmpeg to demux and concat the m4a files
-                tempfile = os.path.join(publications_dir + "/" + article["short_name"], "templist.txt").replace("'", "'\\''")
-
-
+                tempfile = os.path.join(publications_dir + "/" + article["short_name"], "templist.txt").replace("'",
+                                                                                                                "'\\''")
 
                 with open(tempfile, "a") as listf:
 
@@ -139,7 +141,8 @@ def main():
                         sanitized_file = fn["filename"].replace("'", "'\\''")
                         listf.write("file '" + sanitized_file + "'\n")
 
-                concat_command = f"ffmpeg -nostats -loglevel 0 -y -f concat -safe 0 -i \"{tempfile}\" -c copy \"{eventual_outfile}\""
+                concat_command = f"ffmpeg -nostats -loglevel 0 -y -f concat -safe 0 -i \"{tempfile}\" -c copy \"" \
+                                 f"{eventual_outfile}\""
                 os.system(concat_command)
                 # Tagging
                 audio = taglib.File(eventual_outfile)
