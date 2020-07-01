426f991cc9f795ff58ff84bd0b39392fcdd35c4e
jim zhou <43537315+jimtje@users.noreply.github.com>
2020-07-01 12:35:04 -0700

    rework tags, add db to log

--- audm_scraper.py
+++ audm_scraper.py
@@ -3,14 +3,19 @@ import os
 import shutil
 
 import requests
-import taglib
+from mutagen.mp4 import MP4, MP4Cover, MP4Tags
+import dataset
 from alive_progress import alive_bar
+import arrow
 
 config = configparser.RawConfigParser()
 with open("config.cfg", "r") as cfg:
     config.read_file(cfg)
 username = config.get("logins", "username")
 password = config.get("logins", "password")
+
+os.makedirs("output", exist_ok=True)
+db = dataset.connect("sqlite:///output/files.db")
 
 
 class AudmException(BaseException):
@@ -93,13 +98,17 @@ def main():
     audm = Audm(username, password)
     # This grabs the filtering options available. Filtering is first done by narrator, publication, or author.
     filters = audm.filters()
-
+    publication_counter = 1
+    totalpublications = len(filters["publications"])
     for eachpublication in filters["publications"]:
-        print("Publication: " + eachpublication["name_full"])
+        publication_name = eachpublication["name_full"]
+
+
+        print("Publication: " + publication_name + " " + str(publication_counter) + "/" + str(totalpublications))
 
         # I've found it way easier to sort by publication first
-        publications_dir = os.path.abspath("output/" + eachpublication["name_full"])
-        os.makedirs(publications_dir, exist_ok=True)
+        # publications_dir = os.path.abspath("output/" + eachpublication["name_full"])
+        # os.makedirs(publications_dir, exist_ok=True)
 
         articles = audm.articles(publication_ids=[eachpublication["object_id"]])
         article_ids = []
@@ -112,56 +121,88 @@ def main():
         counter = 1
         for article in t["article_versions"]:
             files = []
-            article["publication_name"] = eachpublication["name_full"]
+            article_text = []
+            article["publication_name"] = publication_name
             article_title = article["title"]
             author = article["author_name"].replace('"', '')
+            pubdate = arrow.get(article["pub_date"])
+            article_text_string = ""
+            eventual_outfile = os.path.join("output",
+                                            pubdate.format('YYYY-MM-DD') + "-" + publication_name + "-" + article[
+                                                "short_name"] + ".m4a")
+            illegal_char = ("?", "'", '"', "*", "^", "%", "$", "#", "~", "<", ">", ",", ";", ":", "|",)
+            for char in illegal_char:
+                eventual_outfile = eventual_outfile.replace(char, "")
 
 
-            eventual_outfile = os.path.join(publications_dir,
-                                            article["short_name"] + "-" + author + "-" + article[
-                                                "pub_date"] + ".m4a").replace("'", "'\\''")
-            if not os.path.exists(eventual_outfile):
 
-                print("Article: " + article_title + " by " + author + " " + str(counter) + "/" + str(num_articles))
-                p = audm.paragraphs([article["object_id"]])
-                os.makedirs(publications_dir + "/" + article["short_name"], exist_ok=True)
 
-                # Articles are split up by paragraph and there can be quite a few. Although they are numbered and
-                # timestamped, makes more sense to join them
-                with alive_bar(len(p), force_tty=True) as filebar:
-                    for f in p:
-                        file = audm.get_file(f["audio_filename"])
-                        filename = publications_dir + "/" + article["short_name"] + "/" + f["audio_filename"]
-                        with open(filename, "wb") as fz:
-                            fz.write(file.content)
-                            files.append({"filename": filename, "index": f["index"]})
-                        filebar()
-                fo = sorted(files, key=lambda i: i['index'])
-                # Temporary file to enable ffmpeg to demux and concat the m4a files
-                tempfile = os.path.join(publications_dir + "/" + article["short_name"], "templist.txt").replace("'",
-                                                                                                                "'\\''")
+            if db["articles"].find_one(object_id=article["object_id"]) == None:
 
-                with open(tempfile, "a") as listf:
+                if not os.path.exists(eventual_outfile):
 
-                    for fn in fo:
-                        sanitized_file = fn["filename"].replace("'", "'\\''")
-                        listf.write("file '" + sanitized_file + "'\n")
+                    print("Article: " + article_title + " by " + author + " " + str(counter) + "/" + str(num_articles))
+                    p = audm.paragraphs([article["object_id"]])
+                    os.makedirs("output/" + article["short_name"], exist_ok=True)
 
-                concat_command = f"ffmpeg -nostats -loglevel 0 -y -f concat -safe 0 -i \"{tempfile}\" -c copy \"" \
-                                 f"{eventual_outfile}\""
-                os.system(concat_command)
-                # Tagging
-                audio = taglib.File(eventual_outfile)
-                audio.tags["PERFORMER"] = article["narrator_name"]
-                audio.tags["ARTIST"] = author
-                audio.tags["TITLE"] = article_title
-                audio.tags["ALBUM"] = article["publication_name"]
-                audio.tags["DATE"] = article["pub_date"]
-                audio.tags["DESCRIPTION"] = article["desc"]
-                audio.save()
-                # Cleanup
-                shutil.rmtree(publications_dir + "/" + article["short_name"])
-                counter += 1
+
+                    # Articles are split up by paragraph and there can be quite a few. Although they are numbered and
+                    # timestamped, makes more sense to join them
+                    with alive_bar(len(p), force_tty=True) as filebar:
+                        for f in p:
+                            file = audm.get_file(f["audio_filename"])
+                            filename = os.path.join("output", article["short_name"] + "/" + f["audio_filename"])
+                            with open(filename, "wb") as fz:
+                                fz.write(file.content)
+                                files.append({"filename": filename, "index": f["index"]})
+                                article_text.append({"index": f["index"], "text": f["text"]})
+                            filebar()
+                    fo = sorted(files, key=lambda i: i['index'])
+                    textfo = sorted(article_text, key=lambda i: i['index'])
+
+                    for part in textfo:
+                        article_text_string += part["text"] + "\n"
+                    db["article_text"].insert_ignore(
+                            {"publication": article["publication_name"], "title": article_title, "author": author, "pubdate": pubdate.timestamp, "object_id": article["object_id"], "text": article_text_string}, ["object_id"])
+                    # Temporary file to enable ffmpeg to demux and concat the m4a files
+                    tempfile = os.path.join("output/" + article["short_name"], "templist.txt").replace("'","'\\''")
+
+                    with open(tempfile, "a") as listf:
+
+                        for fn in fo:
+                            listf.write("file '" + os.path.abspath(fn["filename"]) + "'\n")
+                            # -nostats -loglevel 0
+
+                    concat_command = f"ffmpeg -nostats -loglevel 0 -y -f concat -safe 0 -i \"{tempfile}\" -c copy \"" \
+                                     f"{eventual_outfile}\""
+                    os.system(concat_command)
+                    # Tagging
+                    shutil.rmtree("output/" + article["short_name"])
+                    counter += 1
+                    db["articles"].upsert({"publication": article["publication_name"], "title": article_title, "author": author, "pubdate": pubdate.timestamp, "narrator": article["narrator_name"], "description": article["desc"], "object_id": article["object_id"], "file_path": eventual_outfile}, ["object_id"])
+                    audio = MP4(eventual_outfile)
+                    audio.delete()
+                    try:
+                        audio.add_tags()
+                    except:
+                        pass
+                    image = audm.get_file(article["img_lock_2x"])
+                    audio["covr"] = [MP4Cover(data=image.content, imageformat=MP4Cover.FORMAT_PNG)]
+
+                    audio['\xa9nam'] = article_title
+                    audio['\xa9alb'] = publication_name
+                    audio['\xa9ART'] = author
+                    audio['\xa9wrt'] = article["narrator_name"]
+                    audio['\xa9day'] = pubdate.format('YYYY-MM-DD')
+                    audio['desc'] = article["desc"]
+
+                    audio.save()
+                    # Cleanup
+
+                else:
+                    print(article_title + " by " + author + " file exists, skipping article." + " " + str(
+                        counter) + "/" + str(num_articles))
+                    counter += 1
             else:
                 print(article_title + " by " + author + " already downloaded, skipping article." + " " + str(counter) + "/" + str(num_articles))
                 counter += 1
